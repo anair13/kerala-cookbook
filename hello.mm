f9747ee70b8b31cfa76109354f7fa2e74f81ad4f
Ashvin Nair <anair17@berkeley.edu>
2018-07-24 23:28:08 -0700

    Initial ShareLaTeX Import

---
+++ hello.mm
@@ -0,0 +1,24 @@
+<m>
+atiprAcInakAlaMmutalkkutanne shabdan"n"aLuTEyuM
+bhAvahAvAdikaLuTEyuM sahAyattOTukUTi AshayaprakaTanaM
+naTattAn manuSyaR paThiccukazhin~n~irunnu.
+manuSyasharIrattinnuLLilttanneyuLLa h.rdayaM,
+shvAsakOshaM ennI avayavan"n"aLuTe kramaMteRRAteyuLLa
+calanaM avann tALabOdhattinnuLLa AdyapAThan"n"aL
+nalkiyirikkaNaM. atupOlettanne prak.rtiyilninnuM
+avan nityEna kELkkunna saMgItaM---atAyat pakSikaLuTe
+kaLakUjanan"n"aL, kARRinRe maRmmarashabdaM, tEnIccakaLuTe
+mULakkaM, ozhukunna veLLattinRe kaLakaLashabdaM,
+muLan"kATukaLkkiTayilUTe kARRaTikkunpOzhuNTAkunna
+cULaMviLi, m.rgan"n"aLuTe vividharItiyiluLLa shabdan"n"aL
+tuTan"n"iyavayellAM---avanRe saMgItAsvAdanashESiyEyuM
+kramaprav.rddhamAyi vaLaRttikkoNTuvannu. manuSyamastiSkkattinRe
+vaLaRccayuTe tOtanusaricc madhurasvaran"n"aL kETTAsvadikkAnuLLa
+ceviyuTe kazhiv dinaMprati vaLaRnnuvannu. jIvitOpAdhikaL
+sanpAdikkAnuLLa kUTTAya pravaRttanan"n"aLkkiTayil
+AshayavinimayattinRe AvashyakatayuM tanmUlaM bhASayuM
+utbhaviccu vikasiccatupOlettanne jIvitaklEshan"n"aLkkiTayil
+ninnu niR{}v.rtiyuM, kUTTAya addhvAnattinnuLLa pracOdanavuM
+nETENTa AvashyattinnanusariccAN<<+ manuSyanRe
+saMgItaparamAya kazhivukaLuM vaLaRnnu vikasiccat.
+</m>
